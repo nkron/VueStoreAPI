305ea6fe418053ceab154af0d41f8528a3e38f40
Nick Kronberger <nickkronberger@gmail.com>
2020-09-27 11:50:50 -0400

    Max items endpoint

---
+++ VueStoreAPI/Controllers/MaxItemsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VueStore.Repository.Interfaces;
+using VueStore.Repository.Models;
+
+namespace VueStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaxItemsController : ControllerBase
+    {
+        private readonly IItemRepo _itemRepo;
+
+        public MaxItemsController(IItemRepo itemRepo)
+        {
+            _itemRepo = itemRepo;
+        }
+        [HttpGet]
+        public ActionResult<List<Item>> GetMaxPriceItems()
+        {
+            return Ok(_itemRepo.GetMaxPriceItems().Result);
+        }
+        [HttpGet("{name}")]
+        public ActionResult<List<Item>> GetMaxPriceItem(string name)
+        {
+            return Ok(_itemRepo.GetMaxPriceItem(name).Result);
+        }
+    }
+}
